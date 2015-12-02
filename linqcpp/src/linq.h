7782c55d8c0f086d9e1042ca002861caad675473
Eugene Batalov <eabatalov89@gmail.com>
2015-12-03 01:03:57 +0300

    linqcpp exercise code

---
+++ linqcpp/src/linq.h
@@ -0,0 +1,116 @@
+#pragma once
+
+#include <ostream>
+#include <vector>
+#include <functional>
+#include <algorithm>
+#include <initializer_list>
+
+template<class ELEM_TYPE>
+struct enumerable
+{
+    enumerable(const std::vector<ELEM_TYPE> &collection={});
+    enumerable(const enumerable &src) = default;
+    enumerable& operator=(const enumerable &src) = default;
+
+    enumerable& print(std::ostream &out);
+    enumerable where(std::function<bool(const ELEM_TYPE&)> pred) const;
+    template<class DST_TYPE>
+    enumerable<DST_TYPE> select(std::function<DST_TYPE(const ELEM_TYPE&)> transform) const;
+    size_t count() const;
+    size_t count(std::function<bool(const ELEM_TYPE&)> pred) const;
+    bool any() const;
+    bool any(std::function<bool(const ELEM_TYPE&)> pred) const;
+    std::vector<ELEM_TYPE> toVector() const;
+private:
+    std::vector<ELEM_TYPE> collection_;
+};
+
+template<class ELEM_TYPE>
+enumerable<ELEM_TYPE> from(const std::vector<ELEM_TYPE> &collection)
+{
+    return enumerable<ELEM_TYPE>(collection);
+}
+
+template<class ELEM_TYPE>
+enumerable<ELEM_TYPE> from(std::initializer_list<ELEM_TYPE> init_list)
+{
+    return from(std::vector<ELEM_TYPE>(init_list));
+}
+
+template<class ELEM_TYPE>
+enumerable<ELEM_TYPE>::enumerable(const std::vector<ELEM_TYPE> &collection)
+    : collection_(collection)
+{}
+
+template<class ELEM_TYPE>
+auto enumerable<ELEM_TYPE>::print(std::ostream &out) -> enumerable&
+{
+    for (const ELEM_TYPE& elem : collection_) {
+        out << elem << " ";
+    }
+    out << std::endl;
+    return *this;
+}
+
+template<class ELEM_TYPE>
+auto enumerable<ELEM_TYPE>::where(std::function<bool(const ELEM_TYPE&)> pred) const -> enumerable
+{
+    enumerable result(*this);
+    auto first_to_remove = std::remove_if(
+            result.collection_.begin(),
+            result.collection_.end(),
+            std::not1(pred)
+    );
+    result.collection_.erase(
+            first_to_remove,
+            result.collection_.end()
+    );
+    return result;
+}
+
+template<class ELEM_TYPE>
+std::vector<ELEM_TYPE> enumerable<ELEM_TYPE>::toVector() const
+{
+    return collection_;
+}
+
+template<class SRC_TYPE>
+template<class DST_TYPE>
+enumerable<DST_TYPE> enumerable<SRC_TYPE>::select(
+        std::function<DST_TYPE(const SRC_TYPE&)> transform_func) const
+{
+    std::vector<DST_TYPE> result_collection;
+    result_collection.reserve(result_collection.size());
+    std::transform(
+            collection_.begin(),
+            collection_.end(),
+            std::back_inserter(result_collection),
+            transform_func
+    );
+    return enumerable<DST_TYPE>(result_collection);
+}
+
+template<class ELEM_TYPE>
+size_t enumerable<ELEM_TYPE>::count() const
+{
+    return collection_.size();
+}
+
+template<class ELEM_TYPE>
+size_t enumerable<ELEM_TYPE>::count(std::function<bool(const ELEM_TYPE&)> pred) const
+{
+    return std::count_if(collection_.begin(), collection_.end(), pred);
+}
+
+template<class ELEM_TYPE>
+bool enumerable<ELEM_TYPE>::any() const
+{
+    return !collection_.empty();
+}
+
+template<class ELEM_TYPE>
+bool enumerable<ELEM_TYPE>::any(std::function<bool(const ELEM_TYPE&)> pred) const
+{
+    return std::any_of(collection_.begin(), collection_.end(), pred);
+}
---
+++ linqcpp/src/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <functional>
+
+#include "linq.h"
+
+template<class T>
+std::string to_string(const T& val)
+{
+    return std::to_string(val);
+}
+
+int main()
+{
+    using std::vector;
+    using std::cout;
+    using std::endl;
+
+    enumerable<std::string> en =
+         from({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10})
+        .where([](int a) { return a > 5; })
+        .print(cout)
+        .select<int>([](int a) { return a * a; })
+        .print(cout)
+        .select<std::string>(to_string<int>)
+        .print(cout);
+
+    std::cout << en.count() << std::endl;
+    std::cout << en.count([](const std::string &str) { return str == "100"; }) << std::endl;
+    std::cout << en.any() << std::endl;
+    std::cout << en.any([](const std::string &str) { return str == "3"; }) << std::endl;
+
+    auto vec = en.toVector();
+    for (auto elem : vec)
+        std::cout << elem << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
