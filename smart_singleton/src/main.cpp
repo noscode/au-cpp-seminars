4c8160aed3fdffa61c3160865f54abf530b08d0d
Eugene Batalov <eabatalov89@gmail.com>
2016-04-19 12:25:36 +0300

    smart_singletone: small improvement

--- smart_singleton/src/main.cpp
+++ smart_singleton/src/main.cpp
@@ -2,37 +2,45 @@
 #include <iostream>
 #include <cassert>
 
-struct MySingletonService: Singleton<MyService>
+struct my_singleton_service_t: singleton_t<my_singleton_service_t>
 {
     static size_t create_count_;
-    MySingletonService()
+    my_singleton_service_t()
     {
-        std::cout << "MySingletonService::MySingletonService()"
+        std::cout << "my_singleton_service_t::my_singleton_service_t()"
             << std::endl;
         create_count_++;
     }
 
-    ~MySingletonService()
+    ~my_singleton_service_t()
     {
-        std::cout << "MySingletonService::~MySingletonService()"
+        std::cout << "my_singleton_service_t::~my_singleton_service_t()"
             << std::endl;
+    }
+
+    void say_hello()
+    {
+        std::cout << "Hello! I am the " << create_count_
+            << " instance!" << std::endl;
     }
 };
 
-size_t MySingletonService::create_count_ = 0;
-template<> typename Singleton<MySingletonService>::wptr
-    Singleton<MySingletonService>::instance_ = {};
+size_t my_singleton_service_t::create_count_ = 0;
+template<> typename singleton_t<my_singleton_service_t>::wptr
+    singleton_t<my_singleton_service_t>::instance_ = {};
 
 int main()
 {
-    auto p1 = MySingletonService::get_instance();
-    auto p2 = MySingletonService::get_instance();
-    auto p3 = MySingletonService::get_instance();
-    assert(MySingletonService::create_count_ == 1);
+    auto p1 = my_singleton_service_t::get_instance();
+    auto p2 = my_singleton_service_t::get_instance();
+    auto p3 = my_singleton_service_t::get_instance();
+    assert(my_singleton_service_t::create_count_ == 1);
+    p1->say_hello(); p2->say_hello(); p3->say_hello();
     p1.reset(); p2.reset(); p3.reset();
-    assert(MySingletonService::create_count_ == 1);
-    p1 = MySingletonService::get_instance();
-    p2 = MySingletonService::get_instance();
-    assert(MySingletonService::create_count_ == 2);
+    assert(my_singleton_service_t::create_count_ == 1);
+    p1 = my_singleton_service_t::get_instance();
+    p2 = my_singleton_service_t::get_instance();
+    assert(my_singleton_service_t::create_count_ == 2);
+    p1->say_hello(); p2->say_hello();
     return 0;
 }
--- smart_singleton/src/singleton.h
+++ smart_singleton/src/singleton.h
@@ -2,7 +2,7 @@
 #include <memory>
 
 template<class T>
-struct Singleton
+struct singleton_t
 {
     typedef std::shared_ptr<T> ptr;
     static ptr get_instance();
@@ -12,7 +12,7 @@ private:
 };
 
 template<class T>
-auto Singleton<T>::get_instance() -> ptr
+auto singleton_t<T>::get_instance() -> ptr
 {
     ptr p = instance_.lock();
     if (!p)
