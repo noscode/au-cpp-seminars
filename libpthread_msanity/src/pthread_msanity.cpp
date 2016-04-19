342f53254a2f485baf14bcb0a878e5ea4c222803
Eugene Batalov <eabatalov89@gmail.com>
2016-04-19 12:20:11 +0300

    libpthread_msanity: add description for task extension idea

--- libpthread_msanity/src/pthread_msanity.cpp
+++ libpthread_msanity/src/pthread_msanity.cpp
@@ -41,3 +41,15 @@ int pthread_mutex_unlock(pthread_mutex_t *mutex) throw()
     fprintf(stderr, "Unlock %p \n", mutex);
     return original_pthread_mutex_unlock(mutex);
 }
+
+// Looks quite simple.
+// So there is a task for skilled students:
+// implement mutex lock ordering checking.
+// The algorithm is the following:
+// keep each unique sequence
+// of lock operations as ordered array of mutex addresses.
+// Then for each new lock operation find all the locking
+// sequences that contain at least the same locks.
+// Compare current thread locking sequence and all the stored
+// sequences. They should be equal up until and including current
+// mutex address.
